7d05b4a97cfaec286b2748dac9b6ec9abebf0bcc
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-17 00:26:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,8 @@ def bonus
   }
 
   #code your solution here:
-epic_tradegy[:Montague]
+epic_tradedy[:montague][:patriarch][:status]="dead"
+epic_tragedy[:capulet][:patriarch][:status]="dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
